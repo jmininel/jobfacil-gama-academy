c8ddb52c17986f4a7c96b685110cb3cf45c86f8c
jmininel <ju.mininel@hotmail.com>
2021-08-18 12:39:59 -0300

    update home page

--- web/src/pages/Landing/index.jsx
+++ web/src/pages/Landing/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './style.css';
 
-import logoImg from '../../assets/images/pandaLogo.png';
+import logoPanda from '../../assets/images/logo.png'
 
 
  
@@ -11,16 +11,12 @@ export default function Landing() {
     return (
        <div className="container">
          <div className="logo_container">
-             <img className="logoImg" src={logoImg} alt="logo" />
+            <img className="logopanda" src={logoPanda} alt="logo" />
              <h2>O maior portal de empregos, é facil e rápido</h2>
              <h2>Seu proximo emprego esta aqui!</h2>
         </div>
-
-    
-
         
-            
-        <div className="button">
+        <div className="button_class">
             <a href="" className="cadastro">Cadastre-se</a>
         </div>
 
