57b63761f113f6e16bc0f260f8df78676e728e0a
jmininel <ju.mininel@hotmail.com>
2021-08-23 13:47:39 -0300

    created handleSubmit, handleInputChange to Form

--- web/src/pages/Form/index.jsx
+++ web/src/pages/Form/index.jsx
@@ -1,34 +1,54 @@
-import React from 'react';
+import { useState } from 'react';
 
 
 import './style.css'
 
 
  export default function Form() {
-     return (
+       const [formValues, setFormValues] = useState({});
+           
+       const handleInputChange = (e) => {
+           const { name, value } = e.target;
+           setFormValues({...formValues, [name]: value});
+        }
+       
+       const handleSubmit = (e) => {
+           e.preventDefault();
+           const formData = new FormData(e.target);
+           const data = Object.fromEntries(formData);
+
+          console.log('*** handleSubmit', data);
+       };
+
+       console.log('*** formValues',formValues );
+
+    return (
 
      <div id="page-form" className="container-form">
 
       <main>
-         <fieldset>
-           <legend>Dados para cadastro</legend>
+         <form onSubmit={handleSubmit}>
+           <h3>Dados para cadastro</h3>
           
            <div className="input-form">
           <label htmlFor="name">Nome Completo</label>
-          <input type="text" />
+          <input type="text" name="nome" onChange={handleInputChange} value={formValues.nome || ''} />
            </div>
           
 
           <div className="input-form">
              <label htmlFor="job">Cargo Pretendido</label>
-             <input type="text" />
+             <input type="text" name="cargo" onChange={handleInputChange} value={formValues.cargo || ''}  />
           </div>
 
           <div className="input-form">
              <label htmlFor="data-nascimento">Data de Nascimento</label>
-             <input type="text" />
+             <input type="text" name="age" onChange={handleInputChange} value={formValues.age || ''}  />
+             
+             <button type="submit">Enviar</button>
+             
           </div>
-         </fieldset>
+         </form>
      </main>
    </div> 
 
